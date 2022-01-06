1cdfdced2f96002e13791c6b20b59c189fee42f2
Charlie G <welkincz@gmail.com>
2022-01-06 07:52:49 -0500

    Initial Commit

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Charlie G on 2022/1/6.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
